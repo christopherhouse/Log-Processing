13a6b82aa9be37ab72dcf8fc2abb7aba1a3c497e
Chris House <chhouse@microsoft.com>
2023-03-30 14:21:23 -0500

    yeah, I dunno

--- edge/modules/LogProcessing.EdgeCaching/Program.cs
+++ edge/modules/LogProcessing.EdgeCaching/Program.cs
@@ -1,3 +1,6 @@
+using StackExchange.Redis.Extensions.Core.Configuration;
+using StackExchange.Redis.Extensions.Newtonsoft;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var redisConfig = builder.Configuration.GetSection("Redis").Get<RedisConfiguration>();
+
+if (redisConfig == null)
+{
+    throw new ArgumentNullException("Redis config must be present");
+}
+
+builder.Services.AddStackExchangeRedisExtensions<NewtonsoftSerializer>(redisConfig);
 
 var app = builder.Build();
 
